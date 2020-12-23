7af73a3af2354f09bb76c8d828906c8d2009ad79
Vasyl Biliba <58170018+spiritufuerte@users.noreply.github.com>
2020-12-23 20:02:17 +0200

    added routers

---
+++ src/components/Home/Home.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import {NavLink} from "react-router-dom";
+
+const Home = () => {
+    return (
+        <div>
+            dsadasdasdads
+        </div>
+    )
+}
+
+export default Home;
